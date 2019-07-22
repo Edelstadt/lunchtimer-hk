dd04132b3d525d5b317409f07973dbbcd33218e5
David Horacek <h.raven@centrum.cz>
2019-07-22 15:55:58 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-#![feature(async_await, await_macro, futures_api)]
+#![feature(async_await)]
 mod lunches;
 
 use lunches::fetch;
 use std::sync::mpsc::{channel};
+use nickel::{Nickel, HttpRouter, Request, Response, MiddlewareResult};
 
 #[runtime::main]
 async fn main() {
+    let mut server = Nickel::new();
+    server.get("**", lunch);
+    server.listen("127.0.0.1:80").expect("");
+
     let (tx, rx) = channel();
 
     let c = 10;
@@ -13,8 +18,12 @@ async fn main() {
         runtime::spawn(fetch(tx.clone()));
     }
 
-    for i in 0..c { // Await jobs
+    for i in 0..11 { // Await jobs
         dbg!(rx.recv());
     }
+}
+
+fn lunch<'mw>(_req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
+    res.send("Fuck world!")
 }
 
