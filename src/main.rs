d7f33de54b818606d78d58cdc51abf3ed002246d
dlabacek.m <dlabacek.m@hanaboso.com>
2019-07-23 08:22:42 +0200

    middleware

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #![feature(async_await)]
 mod lunches;
-
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-
+#[macro_use] extern crate nickel;
 use lunches::{fetch, r#impl as menus};
 use std::sync::mpsc::{channel};
 use nickel::{Nickel, HttpRouter, Request, Response, MiddlewareResult};
@@ -19,33 +18,40 @@ pub struct Menu {
     body: String,
 }
 
+//static mut p_data: Option<&Vec<Menu>> = None;
 #[runtime::main]
 async fn main() {
     let mut server = Nickel::new();
-    server.get("**", lunch);
 
     let mut data: Vec<Menu> = Vec::new();
-    let p_data = &data;
-    runtime::spawn(update_menus(data));
+//    let p_data: &'static Vec<Menu> = &data;
+    runtime::spawn(update_menus(&mut data));
+
+//    server.get("**", lunch);
+    server.get("**", middleware! { |_, response|
+        let mut data2 = HashMap::new();
+        data2.insert("menus", &data);
+        return response.render("assets/lunches.tpl", &data2)
+    });
 
     server.listen("0.0.0.0:8000").expect("");
 }
 
-fn lunch<'mw>(_req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
-    let menus = vec![
-        Menu{id: 1, title: String::from("Restaurant"), body: String::from("Menu of A ...")},
-        Menu{id: 2, title: String::from("RestaurantB"), body: String::from("Menu of B ...")},
-        Menu{id: 3, title: String::from("RestaurantC"), body: String::from("Menu of C ...")},
-    ];
+//fn lunch<'mw>(_req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
+//    let menus = vec![
+//        Menu{id: 1, title: String::from("Restaurant"), body: String::from("Menu of A ...")},
+//        Menu{id: 2, title: String::from("RestaurantB"), body: String::from("Menu of B ...")},
+//        Menu{id: 3, title: String::from("RestaurantC"), body: String::from("Menu of C ...")},
+//    ];
+//
+//    // Example data
+//    let mut data = HashMap::new();
+//    data.insert("menus", menus);
+//
+//    return res.render("assets/lunches.tpl", &data)
+//}
 
-    // Example data
-    let mut data = HashMap::new();
-    data.insert("menus", menus);
-
-    return res.render("assets/lunches.tpl", &data)
-}
-
-async fn update_menus(mut data: Vec<Menu>) {
+async fn update_menus(data: &'static mut Vec<Menu>) {
     loop {
         // TODO
         let (tx, rx) = channel();
